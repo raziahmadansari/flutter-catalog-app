690aa24ed2381d6b0455e86ebda693a58323ec79
Razi Ahmad Ansari <raziahmad.abd@gmail.com>
2021-11-27 16:28:05 +0530

    created login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:product_catelog/screens/home_page.dart';
 import 'package:product_catelog/screens/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: const HomePage(),
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
       themeMode: ThemeMode.light,
       darkTheme: ThemeData(
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -5,14 +5,56 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Page',
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset('assets/images/login_image.png', fit: BoxFit.cover),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            'Welcome',
             style: TextStyle(
+              fontSize: 24,
               fontWeight: FontWeight.bold,
-              fontSize: 20,
-              color: Colors.blue,
-            )),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'Enter username',
+                    labelText: 'Username',
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  obscuringCharacter: '*',
+                  decoration: const InputDecoration(
+                    hintText: 'Enter password',
+                    labelText: 'Password',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print('clicked');
+                  },
+                  child: const Text('Sign in'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
