b45e14603d3336242aae361adc01e95c4a58ba7c
Razi Ahmad Ansari <raziahmad.abd@gmail.com>
2021-11-27 13:12:42 +0530

    added Scaffold, appBar and drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:product_catelog/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text('Criminal'),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
