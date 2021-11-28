6c131442d1bc118aad41a8f61b82619660424bfd
Razi Ahmad Ansari <raziahmad.abd@gmail.com>
2021-11-28 19:25:02 +0530

    added local json file, loading and json decode

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,28 +1,56 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:product_catelog/models/catalog.dart';
 import 'package:product_catelog/widgets/drawer.dart';
 import 'package:product_catelog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadDate();
+  }
+
+  loadDate() async {
+    await Future.delayed(const Duration(seconds: 5));
+    final catalogJson =
+        await rootBundle.loadString('assets/files/catalog.json');
+    final decodedData = jsonDecode(catalogJson);
+    final productData = decodedData["products"];
+    CatalogModel.items =
+        List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shop'),
       ),
-      body: ListView.builder(
-        itemCount: dummyList.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(
-            item: dummyList[index],
-          );
-        },
-      ),
+      body: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+          ? ListView.builder(
+              itemCount: CatalogModel.items!.length,
+              itemBuilder: (context, index) {
+                return ItemWidget(
+                  item: CatalogModel.items![index],
+                );
+              },
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
       drawer: const AppDrawer(),
     );
   }
